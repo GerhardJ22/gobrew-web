30d7c612f3188299b3b2917fd6da9b7f02ba457c
jon-ajaa <azhardevelop@gmail.com>
2025-03-26 06:05:49 +0700

    perubahan

--- script.js
+++ script.js
@@ -15,5 +15,37 @@ function changeReview() {
     const nextPosition = (currentPosition + 1) % radios.length;
     radios[nextPosition].checked = true;
 }
-
 setInterval(changeReview, 3000);
+
+const getMenu = async () => {
+    const api = 'breweries.json';   
+    try {
+        const response = await fetch(api); 
+        const data = await response.json();
+        displayMenu(data);
+    } catch (error) {
+        console.error("Data Failed to Load:", error); 
+    }
+}
+
+const displayMenu = (data) => {
+    const countries = ['Ireland', 'England', 'Austria'];
+    countries.forEach(country => {
+        const menuContainer = document.getElementById(`menu-${country.toLowerCase()}`);
+        const filteredBreweries = data.filter(brewery => brewery.country === country); 
+        
+        filteredBreweries.forEach(brewery => {
+            const breweryElement = document.createElement('div');
+            breweryElement.classList.add('items-gird');
+            breweryElement.innerHTML = `
+                <a href="${brewery.website_url}" target="_blank">
+                    <img src="${brewery.image}">
+                </a>
+                <div class="item-name">${brewery.name}</div>
+            `;
+            menuContainer.appendChild(breweryElement);
+        });
+    });
+};
+
+getMenu();
