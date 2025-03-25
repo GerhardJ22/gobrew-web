2419bc8fab814db6f4bba431fd5f47d307cca064
Gerhard <gerhard220305@gmail.com>
2025-03-25 14:14:12 +0700

    Connect API

--- script.js
+++ script.js
@@ -26,3 +26,33 @@ function changeReview() {
 }
 
 setInterval(changeReview, 3000);
+
+const getMenu = async () => {
+    const api = 'breweries.json';
+    const response = await fetch(api);
+    const data = await response.json();
+
+    displayMenu(data);
+}
+
+const displayMenu = (data) => {
+    const countries = ['Ireland', 'England', 'Austria'];
+    countries.forEach(country => {
+        const menuContainer = document.getElementById(`menu-${country.toLowerCase()}`);
+        const filteredBreweries = data.filter(brewery => brewery.country === country).slice(0, 5);
+        
+        filteredBreweries.forEach(brewery => {
+            const breweryElement = document.createElement('div');
+            breweryElement.classList.add('items-gird');
+            breweryElement.innerHTML = `
+                <a href="${brewery.website_url}" target="_blank"><img src="${brewery.image}" alt="${brewery.name}"></a>
+                <div class="item-name">${brewery.name}</div>
+                <div class="address">${brewery.address}, ${brewery.city}</div>
+            `;
+            menuContainer.appendChild(breweryElement);
+        });
+    });
+};
+
+getMenu();
+
